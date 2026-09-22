dfe3c61df1bcd70cd2b6b8d946e5337e06a1e889
inhamostova <inha.mostova@gmail.com>
2026-09-23 00:02:56 +0300

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Home } from './Home/Home';
+import { About } from './About/About';
+import { Products } from './Products/Products';
+import { NonFound } from './NonFound/NonFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NonFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/NonFound/NonFound.jsx
@@ -0,0 +1,3 @@
+export const NonFound = () => {
+  return <div>NOT FOUND</div>;
+};
